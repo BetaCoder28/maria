fc6b5a2d6b3b00282bb8755169e17f1cd2de85ca
David Bautista <davidbaar07@gmail.com>
2025-02-10 08:42:18 -0600

    Update Not found page

--- src/Pages/NotFound/index.jsx
+++ src/Pages/NotFound/index.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import NotFoundIcon from '../../assets/svgs/NotFound'
 
 const NotFound = () => {
@@ -7,6 +8,14 @@ const NotFound = () => {
 
                 <p className="text-2xl text-center mt-2"> We are sorry, the page you are looking for is not found. </p>
                 <NotFoundIcon />
+                {/* Regresar al home */}
+                <div className="flex flex-col w-auto items-center h-auto p-6 rounded-lg bg-[#4A235A] shadow-lg border-2 border-white ">
+                        <Link to='/' className="w-full">
+                            <button className="w-full px-4 py-2 bg-[#F4D03F] text-white font-bold rounded transition hover:bg-yellow-600 text-sm md:text-base">
+                                Go to Home
+                            </button>
+                        </Link>
+                    </div>
         </div>
         
     )
