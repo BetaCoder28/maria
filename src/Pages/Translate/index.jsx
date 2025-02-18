1f9a751d3be104f4d77b726cdfc8384c346a37af
David Bautista <davidbaar07@gmail.com>
2025-02-18 09:17:34 -0600

    Functions created to Translator

--- src/Pages/Translate/index.jsx
+++ src/Pages/Translate/index.jsx
@@ -1,4 +1,20 @@
+import {useState } from "react";
+
+
 const Translator = () => {
+
+    const [inputText, setInputText] = useState('');
+    const [translatedText, setTranslatedText] = useState('');
+
+    const clearText = () => {
+        setInputText('');
+    };
+
+    // TODO Función para "traducir" el texto
+    const translateText = () => {
+        setTranslatedText(inputText); 
+    };
+
     return (
         <section className="px-4 py-10 sm:px-6 lg:px-8">
             <div className="mx-auto max-w-screen-xl">
@@ -14,18 +30,22 @@ const Translator = () => {
                                 className="w-full min-h-[200px] md:min-h-[320px] h-auto bg-[#4A235A] p-4 resize-none border-none align-top text-white placeholder-gray-300 focus:ring-0 sm:text-sm"
                                 rows="4"
                                 placeholder="Enter any text..."
+                                value={inputText}
+                                onChange={(e) => setInputText(e.target.value)}
                             ></textarea>
                         </div>
                         <div className="flex flex-col sm:flex-row items-center justify-end gap-2 bg-[#4A235A] p-3">
                             <button
                                 type="button"
                                 className="w-full sm:w-auto rounded-sm bg-gray-200 px-4 py-2 text-sm font-medium text-gray-700 hover:text-white hover:bg-black"
+                                onClick={clearText}
                             >
                                 Clear
                             </button>
                             <button
                                 type="button"
                                 className="w-full sm:w-auto rounded-sm bg-[#F4D03F] px-4 py-2 text-sm font-medium text-white hover:bg-yellow-700"
+                                onClick={translateText}
                             >
                                 Translate
                             </button>
@@ -41,6 +61,8 @@ const Translator = () => {
                                 className="w-full min-h-[200px] md:min-h-[320px] h-auto resize-none border-none align-top text-white placeholder-gray-300 focus:ring-0 sm:text-sm"
                                 rows="4"
                                 placeholder="Translation will appear here..."
+                                value={translatedText}
+                                readOnly
                             ></textarea>
                         </div>
                     </div>
