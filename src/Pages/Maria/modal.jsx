9eda36740d0045fcb04b850fdd5099baa5c2d71b
David Bautista <davidbaar07@gmail.com>
2025-03-14 09:13:10 -0600

    Update modal instructions

--- src/Pages/Maria/modal.jsx
+++ src/Pages/Maria/modal.jsx
@@ -8,10 +8,10 @@ const Modal = ({ isOpen, onClose }) => {
             <div className="bg-[#4A235A] border-2 border-[#F4D03F] rounded-lg p-3 max-w-sm mx-auto">
                 <div className="modal-body">
                     <h5 className="text-lg font-bold">Start a Conversation</h5>
-                    <p className="mt-2">Press the microphone button and start speaking when prompted.</p>
+                    <p className="mt-2">Press the microphone button and start speaking.</p>
                     <hr className="my-4" />
                     <h5 className="text-lg font-bold">Finish the conversation</h5>
-                    <p className="mt-2">To end the conversation say “BYE” and press the microphone button again to deactivate it.</p>
+                    <p className="mt-2">Say “BYE-BYE” to end.</p>
                 </div>
                 <div className="mt-4 flex justify-end">
                     <button onClick={onClose} className="px-4 font-bold py-2 bg-[#F4D03F] text-white rounded hover:bg-yellow-600">
